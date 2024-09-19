2598a7d8cd420bc698c38f05021b15d2473d12e3
vutran <vutharmony@gmail.com>
2024-09-19 02:56:17 -0700

    updated functionalities with targets, rocket,  some effects with explosion

--- rocket-game-canvas/src/store.js
+++ rocket-game-canvas/src/store.js
@@ -4,9 +4,11 @@ import { configureStore, createSlice } from '@reduxjs/toolkit';
 const initialState = {
   isFiring: false,
   rocketPosition: { x: 0, y: 0, rAngle:0 },
+  targets: [],
   launchTime: null,
   holdDuration: 0,
-  tokens: 10, // Initial number of tokens
+  tokens: 10,
+  score: 0 // Initial number of tokens
 };
 
 const gameSlice = createSlice({
@@ -29,10 +31,22 @@ const gameSlice = createSlice({
     resetHoldDuration: (state) => {
       state.holdDuration = 0;
     },
+    removeTargets: (state, action) => {
+      let removeId = action.payload;
+      state.targets[removeId].color = 'red';
+      state.targets[removeId].dead = true;
+      state.score ++;
+    },
+    resetTargets: (state, action) => {
+      state.targets = action.payload;
+    },
+    resetScore: (state) => {
+      state.score = 0;
+    }
   },
 });
 
-export const { startFiring, stopFiring, updateRocketPosition, resetHoldDuration } = gameSlice.actions;
+export const { startFiring, stopFiring, updateRocketPosition, resetHoldDuration, removeTargets, resetTargets, resetScore } = gameSlice.actions;
 
 export const store = configureStore({
   reducer: {
